e932df7da79a5467c0b74ca8d07a458210deed07
Aparna <aparna.chauhan278@gmail.com>
2019-06-09 17:03:30 +0530

    My project setup completed successfully

---
+++ online-shopping-backend/src/main/java/online_shopping/online_shopping_backend/App.java
@@ -0,0 +1,13 @@
+package online_shopping.online_shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
